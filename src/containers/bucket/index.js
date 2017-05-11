3cd2414c3e39771d9491dc5cd342542d52cc8195
orbb1 <orbbmax@gmail.com>
2017-05-11 22:13:41 +0200

    create base structure. Container are ready to extend

---
+++ src/containers/bucket/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Bucket extends Component {
+
+    render() {
+        return <h1>Tha Bucket</h1>
+    }
+}
+
+export default Bucket;
---
+++ src/containers/checkout-form/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class CheckoutForm extends Component {
+
+    render() {
+        return <h1>Here is a Checkout Form</h1>
+    }
+}
+
+export default CheckoutForm;
