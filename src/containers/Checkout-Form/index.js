27b96a8968d2c6813267254259cb2b96c5d07844
orbb1 <orbbmax@gmail.com>
2017-05-14 12:11:06 +0200

    install react-bootstrap

--- src/containers/Checkout-Form/index.js
+++ src/containers/Checkout-Form/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/lib/Button';
+
 import './Checkout-Form.css';
+
 class CheckoutForm extends Component {
 
     render() {
         return (
             <div className="Form-Container">
-                
+                <Button>Button</Button>
             </div>
         )
     }
